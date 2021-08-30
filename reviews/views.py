1339a3335ba6dfdf1e403ef277be4755cea7b701
Bar-Dev <bdprojectservices@gmail.com>
2021-08-30 15:25:13 +0000

    Add reviews and add_reviews pages

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from .models import Review
+from .forms import ReviewForm
 
-# Create your views here.
+
+def reviews(request):
+    if request.method == 'POST':
+        review = ReviewForm(request.POST)
+        if review.is_valid():
+            review.save()
+            messages.info(request, 'Your review has been submitted.')
+            reviews = Review.objects.all()
+            return render(request, 'reviews/reviews.html', {'reviews': reviews})
+    else:
+        reviews = Review.objects.all()
+        context = {
+            'reviews': reviews,
+        }
+    return render(request, 'reviews/reviews.html', context)
+
+
+def add_review(request):
+    """ A view to display form for adding new reviews """
+    if request.method == 'POST':
+        review = ReviewForm(request.POST)
+        if review.is_valid():
+            review.save()
+            messages.info(request, 'Your review has been submitted.')
+            reviews = Review.objects.all()
+            return render(request, 'reviews/reviews.html', {'reviews': reviews})
+    else:
+        reviews = Review.objects.all()
+        context = {
+            'reviews': reviews,
+        }
+    return render(request, 'reviews/add_review.html', context)
